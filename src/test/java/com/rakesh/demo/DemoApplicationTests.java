6280f4715c6ab2acf44aeb936bb06c20f48ff67b
Vijayrao Borade <r.vijayrao.borade@accenture.com>
2022-09-18 15:55:52 +0530

    first one commit

---
+++ src/test/java/com/rakesh/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakesh.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
